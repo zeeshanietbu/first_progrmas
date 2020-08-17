44b0a20fd7ab094eb29c9c692e0ca84854664944
Zeeshan <zeeshanietbu@gmail.com>
2020-08-17 05:00:44 -0700

    This is the leap year program

---
+++ first.c
@@ -0,0 +1,15 @@
+#include<stdio.h>
+void main ()
+{
+    
+    for (int i = 0; i < 50; i++)
+    {
+        /* code */
+        printf("this is the number %d  ",i);
+        printf("\n");
+    }
+    
+    getch();
+}
+
+
