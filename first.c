7ec53c729c4510e83393a279d3e35e88e21b2c2c
Kamran Rasheed <58663501+Kamran-360@users.noreply.github.com>
2020-09-30 05:16:40 -0700

    improved code

--- first.c
+++ first.c
@@ -5,7 +5,7 @@ void main ()
     for (int i = 0; i < 50; i++)
     {
         /* code */
-        printf("this is the number %d  ",i);
+        printf("this is the number %d  ",i); //This is a leap year program
         printf("\n");
     }
     
